02a2de08513dc9303ff0c99f41c18dea1eac89a4
manavk11 <102968214+manavk11@users.noreply.github.com>
2022-05-24 15:22:43 +0530

    Added:HudleColor Extention

--- lib/src/hudle_theme.dart
+++ lib/src/hudle_theme.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:hudle_theme/src/theme_ext.dart';
 
 import 'hudle_colors.dart';
 
@@ -11,19 +11,54 @@ ThemeData hudleTheme({Color scaffoldBackgroundColor = kColorBackground}) {
     primaryColorDark: kColorPrimaryDark,
     //accentColor: kColorAccent,
     indicatorColor: kColorAccent,
-    textSelectionTheme: TextSelectionThemeData(cursorColor: kColorAccent, selectionColor: kColorAccent20),
-    progressIndicatorTheme: ProgressIndicatorThemeData(
-        color: kColorAccent
-    ),
-    colorScheme: ColorScheme.light(primary: kColorPrimary, secondary: kColorAccent),
+    textSelectionTheme: TextSelectionThemeData(
+        cursorColor: kColorAccent, selectionColor: kColorAccent20),
+    progressIndicatorTheme: ProgressIndicatorThemeData(color: kColorAccent),
+    colorScheme:
+        ColorScheme.light(primary: kColorPrimary, secondary: kColorAccent),
     scaffoldBackgroundColor: scaffoldBackgroundColor,
   );
 }
 
-
 AppBarTheme hudleAppBarTheme() {
   return AppBarTheme(
-      systemOverlayStyle : SystemUiOverlayStyle.light,
-      //titleTextStyle: GoogleFonts.roboto()
+    systemOverlayStyle: SystemUiOverlayStyle.light,
+    //titleTextStyle: GoogleFonts.roboto()
   );
+}
+
+ThemeData consumerAppTheme({bool isDark = false}) {
+  return isDark ? _consumerDarkTheme() : _consumerLightTheme();
+}
+
+ThemeData _consumerLightTheme() {
+  return ThemeData(
+    useMaterial3: true,
+    extensions: [
+      HudleColors(
+        primaryText: kPrimaryText,
+        secondaryText: kSecondaryText,
+        colorError: kColorError,
+        colorRating: kColorRating,
+        colorPendingText: kColorPendingText,
+        colorEditTextBorder: kColorEditTextBorder,
+        colorDivider: kColorDivider,
+        tertiaryText: kTertiaryText,
+        colorErrorIcon: kColorError,
+        colorEditTextHint: kColorEditTextHint,
+      )
+    ],
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: SystemUiOverlayStyle.light,
+      actionsIconTheme: IconThemeData(color: kColorAccent),
+      iconTheme: IconThemeData(color: kColorAppBarIcon),
+      backgroundColor: kColorWhite,
+      titleTextStyle: TextStyle(fontSize: 18, color: kColorAppBarTitle),
+      elevation: 0,
+    ),
+  );
+}
+
+ThemeData _consumerDarkTheme() {
+  return ThemeData();
 }
---
+++ lib/src/theme_ext.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class HudleColors extends ThemeExtension<HudleColors> {
+  final Color tertiaryText;
+  final Color colorDivider;
+
+  final Color colorRating;
+  final Color colorEditTextHint;
+  final Color colorEditTextBorder;
+
+  final Color colorErrorIcon;
+  final Color colorPendingText;
+  final Color colorError;
+  final Color primaryText;
+  final Color secondaryText;
+
+  @override
+  HudleColors copyWith(
+      {Color? primaryText,
+      Color? secondaryText,
+      Color? tertiaryText,
+      Color? colorEditTextBorder,
+      Color? colorRating,
+      Color? colorEditTextHint,
+      Color? colorErrorIcon,
+      Color? colorDivider,
+      Color? colorPendingText,
+      Color? colorError}) {
+    return HudleColors(
+      primaryText: primaryText ?? this.primaryText,
+      secondaryText: secondaryText ?? this.secondaryText,
+      tertiaryText: tertiaryText ?? this.tertiaryText,
+      colorEditTextBorder: colorEditTextBorder ?? this.colorEditTextBorder,
+      colorRating: colorRating ?? this.colorRating,
+      colorEditTextHint: colorEditTextHint ?? this.colorEditTextBorder,
+      colorErrorIcon: colorErrorIcon ?? this.colorErrorIcon,
+      colorDivider: colorDivider ?? this.colorDivider,
+      colorPendingText: colorPendingText ?? this.colorPendingText,
+      colorError: colorError ?? this.colorError,
+    );
+  }
+
+  @override
+  HudleColors lerp(ThemeExtension<HudleColors>? other, double t) {
+    if (other is! HudleColors) {
+      return this;
+    }
+    return HudleColors(
+      primaryText: Color.lerp(primaryText, other.primaryText, t) ?? primaryText,
+      secondaryText:
+          Color.lerp(secondaryText, other.secondaryText, t) ?? secondaryText,
+      colorErrorIcon:
+          Color.lerp(colorErrorIcon, other.colorErrorIcon, t) ?? colorErrorIcon,
+      tertiaryText:
+          Color.lerp(tertiaryText, other.tertiaryText, t) ?? tertiaryText,
+      colorEditTextHint:
+          Color.lerp(colorEditTextHint, other.colorEditTextHint, t) ??
+              colorEditTextHint,
+      colorError: Color.lerp(colorError, other.colorError, t) ?? colorError,
+      colorRating: Color.lerp(colorRating, other.colorRating, t) ?? colorRating,
+      colorDivider:
+          Color.lerp(colorDivider, other.colorDivider, t) ?? colorDivider,
+      colorPendingText:
+          Color.lerp(colorPendingText, other.colorPendingText, t) ??
+              colorPendingText,
+      colorEditTextBorder:
+          Color.lerp(colorEditTextBorder, other.colorEditTextBorder, t) ??
+              colorEditTextBorder,
+    );
+  }
+
+  // Optional
+  @override
+  String toString() =>
+      'MyColors(brandColor: $primaryText, danger: $secondaryText)';
+
+  HudleColors(
+      {required this.tertiaryText,
+      required this.colorDivider,
+      required this.colorRating,
+      required this.colorEditTextHint,
+      required this.colorEditTextBorder,
+      required this.colorErrorIcon,
+      required this.colorPendingText,
+      required this.colorError,
+      required this.primaryText,
+      required this.secondaryText});
+}
